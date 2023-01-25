12d1ce97999105fde474d779ccd01977202f519f
ajs3801 <tt14an@gmail.com>
2023-01-25 09:43:51 +0900

    crwn-clothing Setting Up Our Homepage

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,9 +1,12 @@
-import Directory from './components/directory/directory.componenent';
+import { Routes, Route } from 'react-router-dom';
+ 
+import Home from "./Routes/home/home.component";
 
 const App = () => {
   return (
-    <Directory />
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+    </Routes>
   );
-}
-
+};
 export default App;
